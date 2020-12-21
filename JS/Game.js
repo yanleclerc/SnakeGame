0ed81d98fc6adafef80f7678aae98bfef6485ded
yanleclerc <ea091041@ens.uqam.ca>
2020-12-21 16:04:10 -0500

    Bug fixed

---
+++ JS/Game.js
@@ -0,0 +1,42 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED,
+getSnakeHead, snakeIntersection} from './Snake.js'
+import { update as updateFood, draw as drawFood } from './Food.js'
+import { outsideGrid } from "./Grid.js";
+
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById('game-board');
+
+function main(currentTime) {
+    if(gameOver){
+        if(confirm("Game Over,Cliquez sur ok pour recommencer")){
+            window.location='/'
+        }
+    }
+
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime)/ 1000
+    if(secondsSinceLastRender < 1 / SNAKE_SPEED ) return
+
+    lastRenderTime = currentTime;
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+    updateFood()
+    checkDeath()
+}
+
+function draw() {
+    gameBoard.innerHTML=''
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
